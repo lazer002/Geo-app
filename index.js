a65a2ce82a9c0771699bd74b511cedb864e55496
ajitkumar12 <ajitkumar@nicobar.com>
2025-07-03 15:08:20 +0530

    first commit

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-app.use(cors());
 const app = express();
+app.use(cors());
 
 app.use(bodyParser.json());
 
